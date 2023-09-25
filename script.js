483bc74feb8fab8cc15b5a658de4f682ad9efdb0
Sonu-Kaler <130778161+Sonu-Kaler@users.noreply.github.com>
2023-09-25 19:00:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 //your code here
 let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
-console.log(touristSpots);
+
+let articleLessArray =[];
+let regexp= /\bThe\b|\ban\b|\ba\b/g;
+for(let i=0; i<touristSpots.length; i++){
+	let articleLessItem = touristSpots[i].replace(regexp,"").trim();
+	articleLessArray.push(articleLessItem);
+}
+articleLessArray.sort();
+console.log(articleLessArray);
+
+
+
+
