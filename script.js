6f8aef6db63745669ea9f40d270ec500b9a4f765
Sonu-Kaler <130778161+Sonu-Kaler@users.noreply.github.com>
2023-10-12 13:05:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ for(let i=0; i<touristSpots.length; i++){
 	let articleLessItem = touristSpots[i].replace(regexp,"").trim();
 	articleLessArray.push(articleLessItem);
 }
+
 articleLessArray.sort();
+for(let i=0; i<articleLessArray.length; i++){
+var x = document.createElement('li');
+ul.appendChild(li);
+li.innerHTML=articleLessArray[i];
+}
 console.log(articleLessArray);
 
 
